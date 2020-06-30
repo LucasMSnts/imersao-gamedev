cba7c001f08c06c8e8950f766fb80f6baf27955d
Lucas Mauro <lucas_de_mauro@hotmail.com>
2020-06-30 18:47:59 -0300

    Novos inimigos, Pontuação e Pulo duplo

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,16 +1,38 @@
 class Personagem extends Animacao {
-  constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite){
-    super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite);
-    
-    this.yInicial = height - this.altura;    
+  constructor(
+    matriz, 
+    imagem, 
+    x, 
+    variacaoY, 
+    largura, 
+    altura, 
+    larguraSprite, 
+    alturaSprite,
+  ){
+    super(matriz,
+      imagem, 
+      x, 
+      variacaoY, 
+      largura, 
+      altura, 
+      larguraSprite, 
+      alturaSprite
+    );
+    this.variacaoY = variacaoY;    
+    this.yInicial = height - this.altura - this.variacaoY;    
     this.y = this.yInicial
     
     this.velocidadeDoPulo = 0;
-    this.gravidade = 3;
+    this.gravidade = 6;
+    this.alturaDoPulo = -50;
+    this.qtdPulos = 0;
   }
 
   pula(){
-    this.velocidadeDoPulo = -30;
+    if(this.qtdPulos < 2) {
+      this.velocidadeDoPulo = this.alturaDoPulo;
+      this.qtdPulos++;
+    }
   }
   
   aplicaGravidade(){
@@ -19,6 +41,7 @@ class Personagem extends Animacao {
     
     if(this.y > this.yInicial){
       this.y = this.yInicial;
+      this.qtdPulos = 0;
     }
   }
   
